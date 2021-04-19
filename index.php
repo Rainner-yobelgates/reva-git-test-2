343985a4a7ad56300aff3e471b8b3ef1dd8bfc81
Rainner Yobelgates <ryfranceisten@gmail.com>
2021-04-19 16:24:28 +0700

    menambahkan paragraf di inedx.php

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>hello world</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit mollitia, at, modi aspernatur iste numquam in beatae, dolorem recusandae maiores quisquam tempore. Sunt amet numquam facilis. Voluptate quas iusto harum autem eveniet distinctio veniam exercitationem qui quaerat amet accusamus numquam inventore deleniti deserunt suscipit quo, possimus dolor sed at delectus ducimus. Esse beatae libero qui aspernatur enim. Quae recusandae, iste sed at perferendis consectetur? Facere commodi mollitia dolorem optio quod quae corrupti nihil iure molestiae? Voluptatibus consequatur obcaecati praesentium tenetur similique! Eos omnis quis fugiat pariatur dolore, quo distinctio repellat. Cum repudiandae rerum quibusdam sequi ratione beatae consequuntur accusantium expedita?</p>
 </body>
 </html>
